eebea5423ff6e8214c2934a3494352499f25a483
marmah567 <137827687+marmah567@users.noreply.github.com>
2024-02-18 20:16:53 +0100

    created file

--- src/main/resources/static/oblig1.js
+++ src/main/resources/static/oblig1.js
@@ -1,27 +1,46 @@
+
 let movie = document.getElementsByName("movieOption");
 let number = document.getElementsByName("quantity");
 let fname = document.getElementsByName("firstname");
 let lname = document.getElementsByName("lastname");
 let telephone = document.getElementsByName("telephone");
 let email = document.getElementsByName("email");
+
+showTicket();
 let allTickets = [];
-let tickets = {
-    fname, lname,
-    telephone, email,
-    movie, number
+const tickets = {
+    firstname: fname,
+    lastname: lname,
+    telephone: telephone,
+    email: email,
+    movie: movie,
+    number: number
 };
-const bookticket =()=>{
-    allTickets.push(tickets);
-    tickets = {
-        fname, lname,
-        telephone, email,
-        movie, number
-    };
+allTickets = allTickets.push(tickets);
+function showTicket(){
+
+    let out = "<table><tr>" +
+        "<th>Name</th><th>Email</th><th>Telephone</th><th>Movie</th><th>Number of tickets</th>" +
+        "</tr>";
+    for (let t of allTickets){
+        out += "<tr>";
+        out += "<td>"+t.firstname+" "+t.lastname+"</td><td>"+t.email+"</td><td>"+
+            t.telephone+"</td><td>"+t.movie+"</td><td>"+t.number+"</td>";
+        out += "</tr>";
+    }
+    document.getElementById(allTickets).innerHTML = out;
 }
+
+document.getElementsByName("movieOption").value = "";
+document.getElementsByName("quantity").value = "";
+document.getElementsByName("firstname").value = "";
+document.getElementsByName("lastname").value = "";
+document.getElementsByName("telephone").value = "";
+document.getElementsByName("email").value = "";
+
 function deleteticket(){
     deleteticket.addEventListener("click", function (){
-        bookticket.length=0;
-        bookticket();
+        allTickets.length=0;
+        allTickets();
     })
 }
-document.write(allTickets);
